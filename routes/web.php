fa2cfcf6b53ee47794ef23f9005b956bec2b7522
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-06-02 23:02:02 +0530

    feat(web.php) : add permission to routes

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,19 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware('permission:view dashboard')->name('dashboard');
+
+Route::get('/page1',function(){
+    return "page 1";
+})->middleware('permission:view page 1');
+
+Route::get('/page2',function(){
+    return "page 2";
+})->middleware('permission:view page 2');
+
+Route::get('/page3',function(){
+    return "page 3";
+})->middleware('permission:view page 3');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
