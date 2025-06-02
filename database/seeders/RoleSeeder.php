a87b38162e06b6fe060b3616f1c2c4d3fa190a97
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-06-02 22:46:28 +0530

    create roles, permissions and assigned permissions to roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin=Role::create(['name'=>'admin']);
+        $editor=Role::create(['name'=>'editor']);
+        $viewer=Role::create(['name'=>'viewer']);
+
+        $permissions = [
+            'view dashboard',
+            'view page 1',
+            'view page 2',
+            'view page 3',
+        ];
+        foreach($permissions as $permission){
+            Permission::firstOrCreate(['name'=>$permission]);
+        }
+
+        $admin->givePermissionTo(Permission::all());
+
+        $editor->givePermissionTo([
+            'view dashboard',
+            'view page 1',
+            'view page 2',
+        ]);
+
+        $viewer->givePermissionTo([
+            'view dashboard',
+        ]);
+        
+    }
+}
